f7cbe6aeabd35e5fddc88fbedee6e9da38348fd6
snchart <uwu60147@gmail.com>
2025-11-10 21:49:21 +0500

    Fixed Freebsd(i hope)

--- cfetch.c
+++ cfetch.c
@@ -24,9 +24,9 @@ int main(){
 	}
 	if (fgets(buffer, sizeof(buffer), fb) != NULL) {
 		len = strlen(buffer);
-		if (len > 0 && buffer[len - 1] == '\n') {
-			buffer[len - 1] = '\0';
-		}
+			if (len > 0 && buffer[len - 1] == '\n') {
+				buffer[len - 1] = '\0';
+			}
 		if(strcmp(buffer, "FreeBSD") == 0) {
 			char ascia[] = " /\\,-'''''-,/\\  ";
 			char ascib[] = " \\_)	   (_/  ";
@@ -35,38 +35,78 @@ int main(){
 			char ascie[] = "  ;	    ;   ";
 			char ascif[] = "   '-_____-'    ";
 			char ascig[] = "	        ";
+			printf("%s", ascia);
+                        printf(GREEN "OS:     " RESET);
+                        qu;
+                        system("cat /etc/os-release | grep PRETTY_NAME | awk -F '=' '{print $2}' | tr -d '\"\'");
+                        printf("%s", ascib);
+                        printf(GREEN "Host:   " RESET);
+                        qu;
+                        system("hostname");
+                        printf("%s", ascic);
+                        printf(GREEN "Kernel: " RESET);
+                        qu;
+                        system("uname -sr");
+                        printf("%s", ascid);
+                        printf(GREEN "Shell:  " RESET);
+                        qu;
+                        system("echo $SHELL");
+                        printf("%s", ascie);
+                        printf(GREEN "CPU:   " RESET);
+                        qu;
+                        system("sysctl -n hw.model");
+                        printf("%s", ascif);
+                        printf(GREEN "Memory: " RESET);
+                        qu;
+                        system("freecolor -mo | awk 'NR==2{print $3\"MiB\" \" / \" $2\"MiB\"}'");
+                        printf("%s", ascig);
+                        printf(GREEN "Disk:   " RESET);
+                        qu;
+                        system("df -h / | awk 'NR==2 {print $2 \" / \" $3}'");
+                        printf("\n");
+                        return 0;
+
+		} else if(strcmp(buffer, "Linux") == 0) {
+		        char ascia[] = "     .--.     ";
+	        	char ascib[] = "    |o_o |    ";
+        		char ascic[] = "    |:_/ |    ";
+		        char ascid[] = "   //   \\ \\   ";
+		        char ascie[] = "  (|     | )  ";
+		        char ascif[] = " /'\\_   _/'\\  ";
+		        char ascig[] = " \\___)=(___/  ";
+		        printf("%s", ascia);
+		        printf(GREEN "OS:     " RESET);
+        		qu;
+		        system("cat /etc/os-release | grep PRETTY_NAME | awk -F '=' '{print $2}' | tr -d '\"\'");
+		        printf("%s", ascib);
+		        printf(GREEN "Host:   " RESET);
+		        qu;
+		        system("hostname");
+		        printf("%s", ascic);
+		        printf(GREEN "Kernel: " RESET);
+		        qu;
+		        system("uname -sr");
+		        printf("%s", ascid);
+		        printf(GREEN "Shell:  " RESET);
+		        qu;
+		        system("echo $SHELL");
+		        printf("%s", ascie);
+		        printf(GREEN "CPU:   " RESET);
+		        qu;
+		        system("grep 'model name' /proc/cpuinfo | uniq | cut -d ':' -f 2");
+		        printf("%s", ascif);
+		        printf(GREEN "Memory: " RESET);
+		        qu;
+		        system("free -m | awk 'NR==2{print $3 \"MiB\" \" / \" $2 \"MiB\"}'");
+		        printf("%s", ascig);
+		        printf(GREEN "Disk:   " RESET);
+		        qu;
+		        system("df -h / | awk 'NR==2 {print $2 \" / \" $3}'");
+		        printf("\n");
+			return 0;
 		}
 	}
 	pclose(fb);
-	printf("%s", ascia);
-	printf(GREEN "OS:     " RESET);
-	qu;
-	system("cat /etc/os-release | grep PRETTY_NAME | awk -F '=' '{print $2}' | tr -d '\"\'");
-	printf("%s", ascib);
-	printf(GREEN "Host:   " RESET);
-	qu;
-	system("hostname");
-	printf("%s", ascic);
-	printf(GREEN "Kernel: " RESET);
-	qu;
-	system("uname -sr");
-	printf("%s", ascid);
-	printf(GREEN "Shell:  " RESET);
-	qu;
-	system("echo $SHELL");
-	printf("%s", ascie);
-	printf(GREEN "CPU:   " RESET);
-	qu;
-	system("grep 'model name' /proc/cpuinfo | uniq | cut -d ':' -f 2");
-	printf("%s", ascif);
-	printf(GREEN "Memory: " RESET);
-	qu;
-	system("free -m | awk 'NR==2{print $3 \"MiB\" \" / \" $2 \"MiB\"}'");
-	printf("%s", ascig);
-	printf(GREEN "Disk:   " RESET);
-	qu;
-	system("df -h / | awk 'NR==2 {print $2 \" / \" $3}'");
-	printf("\n");
 	return 0;
 }
 
