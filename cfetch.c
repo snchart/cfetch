c79f9a2f26e28e9dcbc53cf64499894ac5a7fe82
snchart <uwu60147@gmail.com>
2025-11-12 20:59:31 +0500

    fixed df

--- cfetch.c
+++ cfetch.c
@@ -66,7 +66,7 @@ int main(){
                         printf("%s", ascig);
                         printf(GREEN "Disk:   " RESET);
                         qu;
-                        system("df -h / | awk 'NR==2 {print $2 \" / \" $3}'");
+                        system("df -h / | awk 'NR==2 {print $3 \" / \" $2}'");
                         printf("\n");
                         return 0;
 
@@ -105,7 +105,7 @@ int main(){
 		        printf("%s", ascig);
 		        printf(GREEN "Disk:   " RESET);
 		        qu;
-		        system("df -h / | awk 'NR==2 {print $2 \" / \" $3}'");
+		        system("df -h / | awk 'NR==2 {print $3 \" / \" $2}'");
 		        printf("\n");
 			return 0;
 		}
